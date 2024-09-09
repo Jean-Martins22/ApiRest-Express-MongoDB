418a42ce6e36ed75c3ae50447b2723d3cecc2a2a
JeanMartins <85572832+Jean-Martins22@users.noreply.github.com>
2024-09-09 17:08:12 -0300

    Criando rota de Busca

--- src/routes/jogosRoutes.js
+++ src/routes/jogosRoutes.js
@@ -4,6 +4,7 @@ import JogoController from "../controllers/jogoController.js";
 const routes = express.Router();
 
 routes.get("/jogos", JogoController.listarJogos);
+routes.get("/jogos/busca", JogoController.listarJogosPorDesenvolvedor);
 routes.get("/jogos/:id", JogoController.listarJogoPorId);
 routes.post("/jogos", JogoController.cadastrarJogo);
 routes.put("/jogos/:id", JogoController.atualizarJogo);
